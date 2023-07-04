6d74a1ba48931c1a986833f6a4d276c955b6774b
rlaxodnjs <xodnjs0876@gmail.com>
2023-07-04 14:26:38 +0900

    ⚙️ :: PackageLibrary

---
+++ WaD/WaD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaD
+//
+//  Created by 김태원 on 2023/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+    }
+
+
+}
+
